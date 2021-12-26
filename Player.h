0849f7112a9a0f52446526769121336b2e436243
inwang <bellwang11@gmail.com>
2021-12-26 18:32:03 -0500

    Player class shells

--- Player.h
+++ Player.h
@@ -1,4 +1,25 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+class Player {
+    private:
+        bool isWhitePlayer;
+        bool isHumanPlayer;
+    public:
+        Player(bool &isWhite_in)
+        : isWhitePlayer(isWhite_in), isHumanPlayer(false) {}
+};
+
+class HumanPlayer : public Player {
+    public:
+        HumanPlayer(bool &isWhite_in)
+        : Player(isWhite_in) {}
+};
+
+class ComputerPlayer: public Player {
+    public:
+        ComputerPlayer(bool &isWhite_in)
+        : Player(isWhite_in) {}
+};
+
 #endif PLAYER_H
